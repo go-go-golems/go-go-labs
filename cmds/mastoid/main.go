fef4ea715c71f35b43fe8e4abcf6973832f31531
Manuel Odendahl <wesen@ruinwesen.com>
2023-07-15 13:50:58 -0400

    :sparkles: Add html output and better parsing of HTML

--- cmds/mastoid/main.go
+++ cmds/mastoid/main.go
@@ -24,6 +24,7 @@ var threadCmd = &cobra.Command{
 		instance, _ := cmd.Flags().GetString("instance")
 		statusID, _ := cmd.Flags().GetString("status-id")
 		verbose, _ := cmd.Flags().GetBool("verbose")
+		html, _ := cmd.Flags().GetBool("html")
 
 		ctx := context.Background()
 
@@ -48,29 +49,37 @@ var threadCmd = &cobra.Command{
 		context, err := client.GetStatusContext(ctx, status.ID)
 		cobra.CheckErr(err)
 
-		printThread(status, context, verbose)
+		printThread(status, context, verbose, html)
 	},
 }
 
-func printStatus(status *mastodon.Status, verbose bool) {
+func printStatus(status *mastodon.Status, verbose bool, html bool) {
 	if verbose {
 		fmt.Printf("Status ID: %s\n", status.ID)
 		fmt.Printf("Created at: %v\n", status.CreatedAt)
-		fmt.Printf("Content: %s\n", convertHTMLToPlainText(status.Content))
+		if html {
+			fmt.Printf("Content: %s\n", status.Content)
+		} else {
+			fmt.Printf("Content: %s\n", convertHTMLToPlainText(status.Content))
+		}
 		fmt.Println("-----------------")
 	} else {
-		fmt.Println(convertHTMLToPlainText(status.Content))
+		if html {
+			fmt.Println(status.Content)
+		} else {
+			fmt.Println(convertHTMLToPlainText(status.Content))
+		}
 	}
 }
 
-func printThread(status *mastodon.Status, context *mastodon.Context, verbose bool) {
-	printStatus(status, verbose)
+func printThread(status *mastodon.Status, context *mastodon.Context, verbose bool, html bool) {
+	printStatus(status, verbose, html)
 
 	for _, ancestor := range context.Ancestors {
 		if verbose {
 			fmt.Println("--AN--")
 		}
-		printStatus(ancestor, verbose)
+		printStatus(ancestor, verbose, html)
 	}
 
 	if verbose {
@@ -81,7 +90,7 @@ func printThread(status *mastodon.Status, context *mastodon.Context, verbose boo
 		if verbose {
 			fmt.Println("--DE--")
 		}
-		printStatus(descendant, verbose)
+		printStatus(descendant, verbose, html)
 	}
 }
 
@@ -92,17 +101,35 @@ func convertHTMLToPlainText(htmlContent string) string {
 	}
 
 	var f func(*html.Node)
-	var output string
+	var output strings.Builder
+	blockTags := map[string]struct{}{
+		"p":       {},
+		"div":     {},
+		"br":      {},
+		"article": {},
+		"section": {},
+		"li":      {},
+	}
+
 	f = func(n *html.Node) {
+		// If the current node is a text node
 		if n.Type == html.TextNode {
-			output += n.Data
+			output.WriteString(n.Data)
 		}
+
+		// If the current node is one of the block tags types
+		if _, present := blockTags[n.Data]; present {
+			output.WriteRune('\n')
+		}
+
+		// Recurse on the children nodes
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			f(c)
 		}
 	}
+
 	f(doc)
-	return output
+	return output.String()
 }
 
 var registerCmd = &cobra.Command{
@@ -192,6 +219,7 @@ func main() {
 	threadCmd.Flags().StringP("instance", "i", "https://hachyderm.io", "Mastodon instance")
 	threadCmd.Flags().StringP("status-id", "s", "", "Status ID")
 	threadCmd.Flags().BoolP("verbose", "v", false, "Verbose output")
+	threadCmd.Flags().Bool("html", false, "HTML output")
 	rootCmd.AddCommand(threadCmd)
 
 	registerCmd.Flags().StringP("client-name", "n", "mastoid", "Client name")
