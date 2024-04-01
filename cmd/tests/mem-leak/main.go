3fe50551db6e9b4cbcd9778d4ae977a62789ab82
Manuel Odendahl <wesen@ruinwesen.com>
2024-04-01 08:59:09 -0400

    :art: Add printing of memory leak information

--- cmd/tests/mem-leak/main.go
+++ cmd/tests/mem-leak/main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
 type Data struct {
 	Text string
+	Blob []byte // Add a slice to hold approximately 100 kB
 }
 
 var dataMap = make(map[int]*Data)
 var counter int
 
 func testFunction() *Data {
-	data := &Data{Text: "This is some text"}
+	// Allocate approximately 100 kB for each Data instance
+	data := &Data{
+		Text: "This is some text",
+		Blob: make([]byte, 100000), // 100,000 bytes ~ 100 kB
+	}
 	return data
 }
 
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MiB\n", m.Alloc/1024/1024)
+}
+
 func main() {
+	// run a goroutine that prints the memory usage every second
+	go func() {
+		for {
+			printMemUsage()
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
 	for {
 		go func() {
 			data := testFunction()
@@ -26,8 +46,8 @@ func main() {
 			// This example does not include synchronization mechanisms for simplicity.
 			dataMap[counter] = data
 			counter++
-			fmt.Println(data)
 		}()
 		time.Sleep(10 * time.Millisecond)
 	}
+
 }
