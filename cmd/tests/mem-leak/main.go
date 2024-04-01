45ba5582d994904acef0f388c67b881933b241d1
Manuel Odendahl <wesen@ruinwesen.com>
2024-04-01 08:53:20 -0400

    :Art: Add actually leaky program

--- cmd/tests/mem-leak/main.go
+++ cmd/tests/mem-leak/main.go
@@ -9,6 +9,9 @@ type Data struct {
 	Text string
 }
 
+var dataMap = make(map[int]*Data)
+var counter int
+
 func testFunction() *Data {
 	data := &Data{Text: "This is some text"}
 	return data
@@ -18,6 +21,11 @@ func main() {
 	for {
 		go func() {
 			data := testFunction()
+			// Protect access to global map and counter with a mutex or similar
+			// to avoid concurrent map writes and race conditions.
+			// This example does not include synchronization mechanisms for simplicity.
+			dataMap[counter] = data
+			counter++
 			fmt.Println(data)
 		}()
 		time.Sleep(10 * time.Millisecond)
