d90cc2bf8f8cfec21da0faf144bc2f4f3bf192b1
Manuel Odendahl <wesen@ruinwesen.com>
2024-04-26 08:25:15 -0400

    :sparkles: Working example

--- cmd/experiments/sqlite-vss/main.go
+++ cmd/experiments/sqlite-vss/main.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"github.com/milosgajdos/go-embeddings"
 	"github.com/milosgajdos/go-embeddings/openai"
-	"log"
+	"github.com/rs/zerolog/log"
 
 	_ "github.com/asg017/sqlite-vss/bindings/go"
 	_ "github.com/mattn/go-sqlite3"
@@ -21,24 +21,95 @@ type Embedder struct {
 	embedder embeddings.Embedder[*openai.EmbeddingRequest]
 }
 
-func (e *Embedder) computeEmbeddings(t string) error {
+func (e *Embedder) indexDocument(title string, body string) error {
 	ctx := context.Background()
 
-	req := &openai.EmbeddingRequest{
-		Input: []string{t, t},
-		Model: "text-embedding-3-small",
-		Dims:  128,
-	}
-
-	embeddings, err := e.embedder.Embed(ctx, req)
+	// check if the title already exists
+	var count int
+	err := e.db.QueryRow("SELECT COUNT(*) FROM documents WHERE title = ?", title).Scan(&count)
 	if err != nil {
 		return err
 	}
 
-	_ = embeddings
+	if count > 0 {
+		log.Info().Msgf("Document with title %s already exists", title)
+		return nil
+	}
 
-	for _, emb := range embeddings {
+	req := &openai.EmbeddingRequest{
+		Input:          []string{title + body},
+		Model:          "text-embedding-3-small",
+		EncodingFormat: "float",
+		Dims:           128,
+	}
+
+	embeddings_, err := e.embedder.Embed(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	for _, emb := range embeddings_ {
 		fmt.Printf("Embedding size: %d\n", len(emb.Vector))
+	}
+
+	if len(embeddings_) != 1 {
+		return fmt.Errorf("expected 1 embedding, got %d", len(embeddings_))
+	}
+
+	// insert document and first embedding as json
+	jsonVector, err := json.Marshal(embeddings_[0].Vector)
+	_, err = e.db.Exec("INSERT INTO documents (body, title, embedding) VALUES (?, ?, ?)", body, title, string(jsonVector))
+
+	// use the inserted row id to insertt he embeddings into the docs virtual table
+	_, err = e.db.Exec("INSERT INTO docs(rowid, embedding) VALUES (last_insert_rowid(), ?)", string(jsonVector))
+
+	return nil
+}
+
+func (e *Embedder) search(question string) error {
+	ctx := context.Background()
+
+	req := &openai.EmbeddingRequest{
+		Input:          []string{question},
+		Model:          "text-embedding-3-small",
+		EncodingFormat: "float",
+		Dims:           128,
+	}
+
+	embeddings_, err := e.embedder.Embed(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	if len(embeddings_) != 1 {
+		return fmt.Errorf("expected 1 embedding, got %d", len(embeddings_))
+	}
+
+	// now use the embedding to do a similarity search
+	jsonVector, err := json.Marshal(embeddings_[0].Vector)
+	rows, err := e.db.Query(`
+		SELECT rowid, distance
+		FROM docs
+		WHERE vss_search(embedding, ?)
+		LIMIT 5
+	`, string(jsonVector))
+
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	// Print the search results
+	fmt.Println("Search Results:")
+	for rows.Next() {
+		var rowid int
+		var distance float64
+		err := rows.Scan(&rowid, &distance)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Document ID: %d, Distance: %.4f\n", rowid, distance)
 	}
 
 	return nil
@@ -54,8 +125,21 @@ func NewEmbedder(f string) (*Embedder, error) {
 }
 
 func (e *Embedder) Init() error {
-	// create a virtual table with embedding(128)
+	// create a documents table with a body and title and embedding column if not exists
 	_, err := e.db.Exec(`
+		CREATE TABLE IF NOT EXISTS documents (
+			id INTEGER PRIMARY KEY,
+			body TEXT,
+			title TEXT,
+			embedding BLOB
+		)
+	`)
+	if err != nil {
+		return err
+	}
+
+	// create a virtual table with embedding(128)
+	_, err = e.db.Exec(`
 		CREATE VIRTUAL TABLE IF NOT EXISTS docs USING vss0(
 			embedding(128)
 		)
@@ -81,7 +165,7 @@ func (e *Embedder) Close() {
 func main() {
 	e, err := NewEmbedder("file:test.db")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("could not create embedder")
 	}
 
 	defer e.Close()
@@ -89,118 +173,56 @@ func main() {
 	fmt.Println(e.VSSVersion())
 	err = e.Init()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("could not initialize embedder")
 	}
 
-	// Open SQLite database
-	db, err := sql.Open("sqlite3", "file:test.db?mode=memory")
+	err = e.indexDocument("A big headline", "Today's news is horrible.")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("could not index document")
 	}
-	defer func(db *sql.DB) {
-		_ = db.Close()
-	}(db)
 
-	return
-	// Load the sqlite-vss extension
-	//err = sqlite_vss.Load(db)
+	err = e.search("what is going on?")
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not search")
+	}
+
+	//// Insert a document embedding
+	//embedding := []float32{0.1, 0.2, 0.3}
+	//embeddingJSON, err := json.Marshal(embedding)
 	//if err != nil {
 	//	log.Fatal(err)
 	//}
-
-	// Create a vss0 virtual table
-	_, err = db.Exec(`
-		CREATE VIRTUAL TABLE docs USING vss0(
-			embedding(3)
-		)
-	`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Insert a document embedding
-	embedding := []float32{0.1, 0.2, 0.3}
-	embeddingJSON, err := json.Marshal(embedding)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = db.Exec("INSERT INTO docs(embedding) VALUES (?)", string(embeddingJSON))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Perform a similarity search
-	query := []float32{0.15, 0.25, 0.35}
-	queryJSON, err := json.Marshal(query)
-	if err != nil {
-		log.Fatal(err)
-	}
-	rows, err := db.Query(`
-		SELECT rowid, distance 
-		FROM docs
-		WHERE vss_search(embedding, ?)
-		LIMIT 5
-	`, string(queryJSON))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	// Print the search results
-	fmt.Println("Search Results:")
-	for rows.Next() {
-		var rowid int
-		var distance float64
-		err := rows.Scan(&rowid, &distance)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Document ID: %d, Distance: %.4f\n", rowid, distance)
-	}
-}
-
-func main2() {
-	db, err := sql.Open("sqlite3", ":memory:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec("create virtual table docs using vss0(embedding(3))")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Insert a document embedding
-	embedding := []float32{0.1, 0.2, 0.3}
-	embeddingJSON, err := json.Marshal(embedding)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = db.Exec("insert into docs(embedding) values(?)", string(embeddingJSON))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Perform a similarity search
-	query := []float32{0.15, 0.25, 0.35}
-	queryJSON, err := json.Marshal(query)
-	if err != nil {
-		log.Fatal(err)
-	}
-	rows, err := db.Query("select rowid, distance from docs where vss_search(embedding, ?)", string(queryJSON))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var rowid int
-		var distance float32
-		err := rows.Scan(&rowid, &distance)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("rowid=%d, distance=%f\n", rowid, distance)
-	}
+	//_, err = db.Exec("INSERT INTO docs(embedding) VALUES (?)", string(embeddingJSON))
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//// Perform a similarity search
+	//query := []float32{0.15, 0.25, 0.35}
+	//queryJSON, err := json.Marshal(query)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//rows, err := db.Query(`
+	//	SELECT rowid, distance
+	//	FROM docs
+	//	WHERE vss_search(embedding, ?)
+	//	LIMIT 5
+	//`, string(queryJSON))
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer rows.Close()
+	//
+	//// Print the search results
+	//fmt.Println("Search Results:")
+	//for rows.Next() {
+	//	var rowid int
+	//	var distance float64
+	//	err := rows.Scan(&rowid, &distance)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	fmt.Printf("Document ID: %d, Distance: %.4f\n", rowid, distance)
+	//}
 }
