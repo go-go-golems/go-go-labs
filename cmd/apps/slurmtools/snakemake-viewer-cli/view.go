10958854531aec0059b1f2af2c7b6582f9e7f0d9
Manuel Odendahl <wesen@ruinwesen.com>
2024-10-18 15:02:20 -0400

    :sparkles: Add job-report options to view

--- cmd/apps/slurmtools/snakemake-viewer-cli/view.go
+++ cmd/apps/slurmtools/snakemake-viewer-cli/view.go
@@ -10,7 +10,9 @@ import (
 	"github.com/go-go-golems/glazed/pkg/cmds/parameters"
 	"github.com/go-go-golems/glazed/pkg/middlewares"
 	"github.com/go-go-golems/glazed/pkg/settings"
+	"github.com/go-go-golems/glazed/pkg/types"
 	"github.com/go-go-golems/go-go-labs/pkg/snakemake"
+	"github.com/go-go-golems/go-go-labs/pkg/jobreports"
 )
 
 type SnakemakeViewerCommand struct {
@@ -18,11 +20,12 @@ type SnakemakeViewerCommand struct {
 }
 
 type SnakemakeViewerSettings struct {
-	LogFiles []string `glazed.parameter:"logfiles"`
-	Verbose  bool     `glazed.parameter:"verbose"`
-	Debug    bool     `glazed.parameter:"debug"`
-	Legacy   bool     `glazed.parameter:"legacy"`
-	Data     string   `glazed.parameter:"data"`
+	LogFiles  []string `glazed.parameter:"logfiles"`
+	Verbose   bool     `glazed.parameter:"verbose"`
+	Debug     bool     `glazed.parameter:"debug"`
+	Legacy    bool     `glazed.parameter:"legacy"`
+	Data      string   `glazed.parameter:"data"`
+	JobReport string   `glazed.parameter:"job-report"`
 }
 
 func NewSnakemakeViewerCommand() (*SnakemakeViewerCommand, error) {
@@ -61,6 +64,11 @@ func NewSnakemakeViewerCommand() (*SnakemakeViewerCommand, error) {
 					parameters.WithDefault("jobs"),
 					parameters.WithChoices("rules", "summary", "jobs", "all"),
 				),
+				parameters.NewParameterDefinition(
+					"job-report",
+					parameters.ParameterTypeString,
+					parameters.WithHelp("Path to the job report file"),
+				),
 			),
 			cmds.WithArguments(
 				parameters.NewParameterDefinition(
@@ -87,10 +95,23 @@ func (c *SnakemakeViewerCommand) RunIntoGlazeProcessor(
 		return err
 	}
 
+	var jobReportData *jobreports.ReportData
+	if s.JobReport != "" {
+		var err error
+		jobReportData, err = jobreports.ParseJobReportFile(s.JobReport)
+		if err != nil {
+			return fmt.Errorf("error parsing job report file %s: %w", s.JobReport, err)
+		}
+	}
+
 	for _, logFile := range s.LogFiles {
 		logData, err := snakemake.ParseLog(logFile, s.Debug)
 		if err != nil {
 			return fmt.Errorf("error parsing log file %s: %w", logFile, err)
+		}
+
+		if jobReportData != nil {
+			logData = c.joinJobReportData(logData, jobReportData)
 		}
 
 		if s.Legacy {
@@ -105,6 +126,21 @@ func (c *SnakemakeViewerCommand) RunIntoGlazeProcessor(
 	}
 
 	return nil
+}
+
+func (c *SnakemakeViewerCommand) joinJobReportData(logData snakemake.LogData, jobReportData *jobreports.ReportData) snakemake.LogData {
+	jobMap := make(map[string]*jobreports.Job)
+	for _, job := range jobReportData.Jobs {
+		jobMap[job.ID] = job
+	}
+
+	for i, job := range logData.Jobs {
+		if reportJob, ok := jobMap[job.ID]; ok {
+			logData.Jobs[i].JobReport = reportJob
+		}
+	}
+
+	return logData
 }
 
 func (c *SnakemakeViewerCommand) legacyOutput(logData snakemake.LogData, verbose bool, dataType string, filename string) error {
@@ -162,6 +198,19 @@ func (c *SnakemakeViewerCommand) legacyOutput(logData snakemake.LogData, verbose
 			if job.ScannerError != "" {
 				fmt.Printf("  Scanner Error: %s\n", job.ScannerError)
 			}
+			// Add job report data if available
+			if job.JobReport != nil {
+				fmt.Printf("  User: %s\n", job.JobReport.User)
+				fmt.Printf("  Account: %s\n", job.JobReport.Account)
+				fmt.Printf("  Partition: %s\n", job.JobReport.Partition)
+				fmt.Printf("  CPUs: %d\n", job.JobReport.CPUs)
+				fmt.Printf("  RAM: %.2f GB\n", job.JobReport.RAM)
+				fmt.Printf("  GPUs: %d\n", job.JobReport.GPUs)
+				fmt.Printf("  Pending Time: %s\n", job.JobReport.PendingTime)
+				fmt.Printf("  CPU Efficiency: %.2f%%\n", job.JobReport.CPUEfficiency)
+				fmt.Printf("  RAM Efficiency: %.2f%%\n", job.JobReport.RAMEfficiency)
+				fmt.Printf("  Wall Time Efficiency: %.2f%%\n", job.JobReport.WallTimeEfficiency)
+			}
 			fmt.Println()
 		}
 	}
@@ -170,5 +219,105 @@ func (c *SnakemakeViewerCommand) legacyOutput(logData snakemake.LogData, verbose
 }
 
 func (c *SnakemakeViewerCommand) structuredOutput(ctx context.Context, gp middlewares.Processor, logData snakemake.LogData, verbose bool, dataType string, filename string) error {
-	return snakemake.OutputLogDataToGlazedProcessor(ctx, gp, logData, verbose, dataType, filename)
+	if dataType == "summary" || dataType == "all" {
+		err := gp.AddRow(ctx, types.NewRowFromMap(map[string]interface{}{
+			"total_jobs":      logData.TotalJobs,
+			"completed_jobs":  logData.Completed,
+			"in_progress_jobs": logData.InProgress,
+			"filename":        filename,
+		}))
+		if err != nil {
+			return err
+		}
+	}
+
+	if dataType == "rules" || dataType == "all" {
+		for ruleName, rule := range logData.Rules {
+			completedJobs := 0
+			var totalDuration time.Duration
+			for _, job := range rule.Jobs {
+				if job.Status == snakemake.StatusCompleted {
+					completedJobs++
+					totalDuration += job.Duration
+				}
+			}
+
+			avgDuration := time.Duration(0)
+			if completedJobs > 0 {
+				avgDuration = totalDuration / time.Duration(completedJobs)
+			}
+
+			ruleRow := types.NewRowFromMap(map[string]interface{}{
+				"rule_name":       ruleName,
+				"total_jobs":      len(rule.Jobs),
+				"completed_jobs":  completedJobs,
+				"average_duration": avgDuration.String(),
+				"filename":        filename,
+			})
+
+			for _, resource := range rule.Resources {
+				ruleRow.Set(fmt.Sprintf("resource_%s", resource.Name), resource.Value)
+			}
+
+			if err := gp.AddRow(ctx, ruleRow); err != nil {
+				return err
+			}
+		}
+	}
+
+	if dataType == "jobs" || dataType == "all" {
+		for _, job := range logData.Jobs {
+			jobRow := types.NewRowFromMap(map[string]interface{}{
+				"job_id":     job.ID,
+				"rule":       job.Rule,
+				"start_time": job.StartTime,
+				"end_time":   job.EndTime,
+				"duration":   job.Duration.String(),
+				"status":     string(job.Status),
+				"filename":   filename,
+			})
+
+			// Add job report data if available
+			if job.JobReport != nil {
+				jobRow.Set("jobreport_id", job.JobReport.ID)
+				jobRow.Set("jobreport_user", job.JobReport.User)
+				jobRow.Set("jobreport_account", job.JobReport.Account)
+				jobRow.Set("jobreport_partition", job.JobReport.Partition)
+				jobRow.Set("jobreport_status", string(job.JobReport.Status))
+				jobRow.Set("jobreport_start_time", job.JobReport.StartTime)
+				jobRow.Set("jobreport_wall_time", job.JobReport.WallTime.String())
+				jobRow.Set("jobreport_run_time", job.JobReport.RunTime.String())
+				jobRow.Set("jobreport_cpus", job.JobReport.CPUs)
+				jobRow.Set("jobreport_ram", job.JobReport.RAM)
+				jobRow.Set("jobreport_gpus", job.JobReport.GPUs)
+				jobRow.Set("jobreport_pending_time", job.JobReport.PendingTime.String())
+				jobRow.Set("jobreport_cpu_efficiency", job.JobReport.CPUEfficiency)
+				jobRow.Set("jobreport_ram_efficiency", job.JobReport.RAMEfficiency)
+				jobRow.Set("jobreport_wall_time_efficiency", job.JobReport.WallTimeEfficiency)
+			}
+
+			for _, resource := range job.Resources {
+				jobRow.Set(fmt.Sprintf("resource_%s", resource.Name), resource.Value)
+			}
+
+			if verbose {
+				jobRow.Set("input", job.Input)
+				jobRow.Set("output", job.Output)
+				jobRow.Set("reason", job.Reason)
+				for field, value := range job.Details {
+					jobRow.Set(fmt.Sprintf("detail_%s", field), value)
+				}
+			}
+
+			if job.ScannerError != "" {
+				jobRow.Set("scanner_error", job.ScannerError)
+			}
+
+			if err := gp.AddRow(ctx, jobRow); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
