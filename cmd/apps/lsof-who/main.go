c070e0f801b572b8a82af998012c3a4171af51a5
Manuel Odendahl <wesen@ruinwesen.com>
2025-06-06 15:38:33 -0700

    :sparkles: Add kill flag

--- cmd/apps/lsof-who/main.go
+++ cmd/apps/lsof-who/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/shirou/gopsutil/v3/net"
 	"github.com/shirou/gopsutil/v3/process"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 type ProcessInfo struct {
@@ -119,11 +120,32 @@ func gatherInfo(p *process.Process) (*ProcessInfo, error) {
 	return info, nil
 }
 
+func killProcessBySignal(pid int32, signalType string) error {
+	var sig syscall.Signal
+	switch strings.ToUpper(signalType) {
+	case "TERM":
+		sig = syscall.SIGTERM
+	case "KILL":
+		sig = syscall.SIGKILL
+	case "INT":
+		sig = syscall.SIGINT
+	default:
+		sig = syscall.SIGTERM
+	}
+
+	return syscall.Kill(int(pid), sig)
+}
+
+func isTerminal() bool {
+	return term.IsTerminal(int(os.Stdin.Fd()))
+}
+
 func main() {
 	var port uint32
 	var outputJSON bool
 	var killProcess bool
 	var signalType string
+	var interactive bool
 
 	rootCmd := &cobra.Command{
 		Use:   "portinfo [flags]",
@@ -136,6 +158,19 @@ func main() {
 			info, err := gatherInfo(p)
 			if err != nil {
 				log.Fatalln(err)
+			}
+
+			// If kill flag is set, kill immediately without prompting
+			if killProcess {
+				fmt.Printf("Killing process %d (%s) with signal %s\n", info.PID, info.Name, signalType)
+				err := killProcessBySignal(info.PID, signalType)
+				if err != nil {
+					log.Printf("Failed to kill process: %v\n", err)
+					os.Exit(1)
+				} else {
+					fmt.Printf("Signal %s sent to process %d successfully\n", signalType, info.PID)
+				}
+				return
 			}
 
 			if outputJSON {
@@ -170,65 +205,58 @@ func main() {
 				}
 			}
 
-			// Create a form to ask the user if they want to kill the process and select a signal
-			form := huh.NewForm(
-				huh.NewGroup(
-					huh.NewSelect[bool]().
-						Title("Do you want to kill the process?").
-						Options(
-							huh.NewOption("Yes", true),
-							huh.NewOption("No", false),
-						).
-						Value(&killProcess),
-					huh.NewSelect[string]().
-						Title("Select signal type").
-						Options(
-							huh.NewOption("TERM", "TERM"),
-							huh.NewOption("KILL", "KILL"),
-							huh.NewOption("INT", "INT"),
-						).
-						Value(&signalType),
-				),
-			)
+			// Only show interactive form if we're on a TTY or --interactive flag is set
+			if interactive || isTerminal() {
+				// Create a form to ask the user if they want to kill the process and select a signal
+				var interactiveKill bool
+				var interactiveSignal string
+				form := huh.NewForm(
+					huh.NewGroup(
+						huh.NewSelect[bool]().
+							Title("Do you want to kill the process?").
+							Options(
+								huh.NewOption("Yes", true),
+								huh.NewOption("No", false),
+							).
+							Value(&interactiveKill),
+						huh.NewSelect[string]().
+							Title("Select signal type").
+							Options(
+								huh.NewOption("TERM", "TERM"),
+								huh.NewOption("KILL", "KILL"),
+								huh.NewOption("INT", "INT"),
+							).
+							Value(&interactiveSignal),
+					),
+				)
 
-			// Run the form
-			err = form.Run()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// Implement logic based on user input
-			if killProcess {
-				// Map signal string to syscall signal
-				var sig syscall.Signal
-				switch strings.ToUpper(signalType) {
-				case "TERM":
-					sig = syscall.SIGTERM
-				case "KILL":
-					sig = syscall.SIGKILL
-				case "INT":
-					sig = syscall.SIGINT
-				default:
-					sig = syscall.SIGTERM
-				}
-
-				log.Printf("Killing process %d (%s) with signal %s\n", info.PID, info.Name, signalType)
-
-				// Send the signal to the process
-				err := syscall.Kill(int(info.PID), sig)
+				// Run the form
+				err = form.Run()
 				if err != nil {
-					log.Printf("Failed to kill process: %v\n", err)
-				} else {
-					log.Printf("Signal %s sent to process %d successfully\n", signalType, info.PID)
+					log.Fatal(err)
 				}
-			} else {
-				log.Println("Process will not be killed.")
+
+				// Implement logic based on user input
+				if interactiveKill {
+					fmt.Printf("Killing process %d (%s) with signal %s\n", info.PID, info.Name, interactiveSignal)
+					err := killProcessBySignal(info.PID, interactiveSignal)
+					if err != nil {
+						log.Printf("Failed to kill process: %v\n", err)
+					} else {
+						fmt.Printf("Signal %s sent to process %d successfully\n", interactiveSignal, info.PID)
+					}
+				} else {
+					fmt.Println("Process will not be killed.")
+				}
 			}
 		},
 	}
 
 	rootCmd.Flags().Uint32VarP(&port, "port", "p", 0, "port number to inspect")
 	rootCmd.Flags().BoolVarP(&outputJSON, "json", "j", false, "output as JSON")
+	rootCmd.Flags().BoolVarP(&killProcess, "kill", "k", false, "kill the process immediately without prompting")
+	rootCmd.Flags().StringVarP(&signalType, "signal", "s", "TERM", "signal to send (TERM, KILL, INT)")
+	rootCmd.Flags().BoolVarP(&interactive, "interactive", "i", false, "force interactive mode even when not on a TTY")
 	rootCmd.MarkFlagRequired("port")
 
 	if err := rootCmd.Execute(); err != nil {
